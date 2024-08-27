e0f499b8c22ae38b063fdc3432319876196d6042
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-08-27 11:51:36 +0200

    [UPD] installed package for flags

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -3,17 +3,19 @@
         <input type="text" v-model="searchQuery">
         <button type="button" @click="getMoviesList()"><i class="fa-solid fa-magnifying-glass"></i></button>
     </div>
-    <ul v-for="movie, index in store.moviesList" :key="index">
+    <ol v-for="movie, index in store.moviesList" :key="index">
         <li>{{ movie.title }}</li>
         <li>{{ movie.original_title }}</li>
-        <li>{{ movie.original_language }}</li>
+        <li><span :class="`fi fi-${movie.original_language}`"></span>  {{ movie.original_language }}</li>
         <li>{{ movie.vote_average }}</li>
-    </ul>
+    </ol>
+    <span class="fi fi-en"></span>
 </template>
 
 <script>
 import { store } from '../store';
 import axios from 'axios';
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 export default {
     data() {
@@ -33,5 +35,11 @@ export default {
 </script>
 
 <style scoped>
-    
+    li {
+        margin: 5px 25px;
+    }
+
+    li:first-child {
+        border-top: 1px solid black;
+    }
 </style>
