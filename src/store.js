00ed07d83db5dd8c2d39fe10326297debfce9cd1
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-08-27 10:36:14 +0200

    [UPD] created project and milestone 0 done

---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    apiUrlMovie: "https://api.themoviedb.org/3/search/movie?api_key=23534135ecaf0f022b163c9be897d83b",
+    moviesList: {}
+})
