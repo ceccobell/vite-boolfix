f131b47b7fd3f288e7220a1f9b32cfdccd6468a5
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-08-27 18:32:06 +0200

    [UPD] milestone 4 done

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -1,32 +1,4 @@
 <template>
-    <!-- <div class="searchbar">
-        <input type="text" v-model="searchQuery">
-        <button type="button" @click="getMoviesList(), getSerieList()"><i class="fa-solid fa-magnifying-glass"></i></button>
-    </div>
-    <h1>Lista dei film:</h1>
-    <ol v-for="(movie, index) in store.moviesList" :key="index">
-        <li>{{ movie.title }}</li>
-        <li>{{ movie.original_title }}</li>
-        <li>
-            <span :class="`fi fi-${convertLanguageCode(movie.original_language)}`"></span>
-            {{ movie.original_language }}
-        </li>
-        <li> 
-            <i v-for="star, index in stars" :class="index < convertVote(movie.vote_average) ? 'fa-solid fa-star' : 'fa-regular fa-star'"></i>
-        </li>
-        <li><img :src="`http://image.tmdb.org/t/p/w92/${movie.poster_path}`" alt=""></li>
-    </ol>
-    <h1>Lista delle Serie TV:</h1>
-    <ol v-for="(serie, index) in store.serieTV_list" :key="index">
-        <li>{{ serie.name }}</li>
-        <li>{{ serie.original_name }}</li>
-        <li>
-            <span :class="`fi fi-${convertLanguageCode(serie.original_language)}`"></span>
-            {{ serie.original_language }}
-        </li>
-        <li><i v-for="star, index in stars" :class="index < convertVote(serie.vote_average) ? 'fa-solid fa-star' : 'fa-regular fa-star'"></i></li>
-        <li><img :src="`http://image.tmdb.org/t/p/w92/${serie.poster_path}`" alt=""></li>
-    </ol> -->
     <div class="container">
         <div class="row bg-black">
             <div class="col-50">
