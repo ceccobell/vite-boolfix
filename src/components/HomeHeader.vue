5b0d22153f3696cb779d622260c402b60f0c0db7
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-09-16 17:23:55 +0200

    [UPD] new style to header

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -1,106 +1,141 @@
+<script>
+import { store } from "../store"
+import axios from "axios"
+import "/node_modules/flag-icons/css/flag-icons.min.css"
+
+export default {
+    data() {
+        return {
+            store,
+            searchActive: false, // stato per attivare/disattivare il campo di input
+            searchQuery: "", // variabile per memorizzare il valore della ricerca
+            navbarItems: [
+                { name: "Home", url: "/", selected: true },
+                { name: "Serie Tv", url: "/tv", selected: false },
+                { name: "Film", url: "/film", selected: false },
+                { name: "Nuovi e Popolari", url: "/nuoviPopolari", selected: false },
+                { name: "La mia lista", url: "/lamialista", selected: false },
+                { name: "Sfoglia per lingua", url: "/sfogliaperlingua", selected: false },
+            ],
+        }
+    },
+    methods: {
+        toggleSearch() {
+            this.searchActive = !this.searchActive
+            if (!this.searchActive) {
+                this.searchQuery = "" // resetta il campo input se chiuso
+            }
+        },
+        performSearch() {
+            if (this.searchQuery.trim()) {
+                // Solo se c'è una query
+                axios.get(store.apiUrlMovies + this.searchQuery).then((result) => {
+                    store.moviesList = result.data.results
+                })
+                axios.get(store.apiUrlSerieTV + this.searchQuery).then((result) => {
+                    store.serieTV_list = result.data.results
+                })
+            }
+        },
+        convertLanguageCode(languageCode) {
+            if (languageCode === "en") {
+                languageCode = "gb"
+            }
+            return languageCode
+        },
+    },
+}
+</script>
+
 <template>
     <div class="container">
         <div class="row bg-black">
-            <div class="col-50">
-                <h1>BOOLFLIX</h1>
-            </div>
-            <div class="col-50 d-flex align-items-center justify-content-end">
-                <div class="input-group my-10">
-                    <input 
-                        type="text" 
-                        class="form-control" 
-                        placeholder="Cerca Film, Serie..." 
-                        aria-label="Player's name" 
-                        aria-describedby="search-button" 
-                        v-model="store.searchQuery" 
-                    />
-                    <button class="btn" type="button" @click="getMoviesList(), getSerieList()">
+            <div class="col-100 d-flex justify-content-between align-items-center">
+                <div class="d-flex align-items-center">
+                    <h1>BOOLFLIX</h1>
+                    <ul class="list-unstyled d-flex">
+                        <li
+                            class="text-white"
+                            v-for="(item, index) in navbarItems"
+                            :key="index"
+                            :value="index + 1">
+                            {{ item.name }}
+                        </li>
+                    </ul>
+                </div>
+                <div class="search-wrapper">
+                    <!-- Icona della lente -->
+                    <button class="search-btn" type="button" @click="toggleSearch">
                         <i class="fa-solid fa-magnifying-glass text-white"></i>
                     </button>
+
+                    <!-- Campo di input che appare quando searchActive è true -->
+                    <input
+                        v-if="searchActive"
+                        type="text"
+                        v-model="searchQuery"
+                        class="form-control search-input"
+                        placeholder="Cerca film o serie..."
+                        @keyup.enter="performSearch" />
                 </div>
             </div>
         </div>
     </div>
 </template>
 
-<script>
-import { store } from '../store';
-import axios from 'axios';
-import "/node_modules/flag-icons/css/flag-icons.min.css";
-
-export default {
-    data() {
-        return {
-            store
-        }
-    },
-    methods: {
-        getMoviesList() {
-            axios.get(store.apiUrlMovies + store.searchQuery).then((result) => {
-                store.moviesList = result.data.results;
-            });
-        },
-        getSerieList() {
-            axios.get(store.apiUrlSerieTV + store.searchQuery).then((result) => {
-                store.serieTV_list = result.data.results;
-            });
-        },
-        convertLanguageCode(languageCode) {
-            // Se il codice lingua è 'en', lo converte in 'gb'
-            if (languageCode === 'en') {
-                languageCode = 'gb';
-            }
-            return languageCode;
-        }
-    }
-}
-</script>
-
 <style scoped>
-    li {
-        margin: 5px 25px;
-    }
+li {
+    margin: 5px 25px;
+    font-size: var(--font-size-sm);
+}
 
-    li:first-child {
-        border-top: 1px solid black;
-    }
+li:first-child {
+    border-top: 1px solid black;
+}
 
-    .fa-star {
-        color: #FFD43B;
-    }
+.col-100 {
+    padding: 10px;
+}
 
-    .col-50 {
-        padding: 10px;
-    }
+h1 {
+    color: #e50914;
+    font-size: 30px;
+    margin-right: 15px;
+}
 
-    h1 {
-        color: red;
-    }
+.search-wrapper {
+    display: flex;
+    align-items: center;
+    position: relative;
+}
 
-    .input-group {
-        display: flex;
-        width: 70%;
-        margin: 0 auto;
-        position: relative;
-    }
+.search-btn {
+    background: none;
+    border: none;
+    color: white;
+    font-size: var(--font-size-xl);
+    cursor: pointer;
+}
 
-    .form-control {
-        flex: 1 1 auto;
-        padding: 10px;
-        font-size: 16px;
-        border: 1px solid #ced4da;
-        border-radius: 10px 0 0 10px;
-        background-color: #e9ecef;
-        position: relative;
-    }
+.search-input {
+    width: 0;
+    opacity: 0;
+    padding: 8px;
+    font-size: 16px;
+    border: 1px solid #ced4da;
+    border-radius: 10px;
+    background-color: #e9ecef;
+    transition: all 0.3s ease;
+    margin-left: 10px;
+}
 
-    .btn {
-        display: inline-block;
-        padding: 10px 16px;
-        font-size: 16px;
-        text-align: center;
-        cursor: pointer;
-        border: 0;
-        border-radius: 0 10px 10px 0;
-    }
+.search-input::placeholder {
+    color: #6c757d;
+}
+
+.search-wrapper input {
+    width: 200px;
+    opacity: 1;
+    transition: width 0.3s ease, opacity 0.3s ease;
+}
 </style>
--- src/components/HomeMain.vue
+++ src/components/HomeMain.vue
@@ -1,15 +1,21 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-25" v-for="movie, index in store.moviesList">
+            <div class="col-25" v-for="(movie, index) in store.moviesList">
                 <div class="card">
-                    <img :src="`http://image.tmdb.org/t/p/w342/${movie.poster_path}`" alt="">
+                    <img :src="`http://image.tmdb.org/t/p/w342/${movie.poster_path}`" alt="" />
                     <ul>
                         <li>Titolo: {{ movie.title }}</li>
                         <li>Titolo originale: {{ movie.original_title }}</li>
                         <li>
-                            Voto: 
-                            <i v-for="star, index in stars" :class="index < convertVote(movie.vote_average) ? 'fa-solid fa-star' : 'fa-regular fa-star'"></i>
+                            Voto:
+                            <i
+                                v-for="(star, index) in stars"
+                                :class="
+                                    index < convertVote(movie.vote_average)
+                                        ? 'fa-solid fa-star'
+                                        : 'fa-regular fa-star'
+                                "></i>
                         </li>
                         <li>Overview: {{ movie.overview }}</li>
                     </ul>
@@ -20,47 +26,47 @@
 </template>
 
 <script>
-import { store } from '../store';
+import { store } from "../store"
 
 export default {
     data() {
         return {
             store,
-            stars: 5
+            stars: 5,
         }
     },
     methods: {
         convertVote(vote_average) {
             return Math.round(vote_average / 2)
-        }
-    }
+        },
+    },
 }
 </script>
 
 <style scoped>
-    .card {
-        padding: 10px;
-    }
+.card {
+    padding: 10px;
+}
 
-    img {
-        width: 100%;
-        height: auto;
-    }
+img {
+    width: 100%;
+    height: auto;
+}
 
-    ul {
-        list-style-type: none;
-        display: none;
-    }
+ul {
+    list-style-type: none;
+    display: none;
+}
 
-    .card:hover img {
-        display: none;
-    }
+.card:hover img {
+    display: none;
+}
 
-    .card:hover ul {
-        display: block;
-    }
+.card:hover ul {
+    display: block;
+}
 
-    .fa-star {
-        color: #FFD43B;
-    }
+.fa-star {
+    color: #ffd43b;
+}
 </style>
