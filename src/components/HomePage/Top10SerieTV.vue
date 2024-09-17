86f42a8a92ed5ad38feacd0509a6dfd8f61fa7fd
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-09-17 09:46:24 +0200

    [UPD] added graphics for top10

--- src/components/HomePage/Top10SerieTV.vue
+++ src/components/HomePage/Top10SerieTV.vue
@@ -12,7 +12,11 @@ export default {
 <template>
     <div class="container">
         <div class="row">
-            <div v-for="(serie, index) in store.top10serieTV_list" :key="serie.id" class="col-25">
+            <div
+                v-for="(serie, index) in store.top10serieTV_list"
+                :key="serie.id"
+                class="col-25 d-flex">
+                <img :src="`/grafiche/${index + 1}.png`" alt="" />
                 <img :src="`http://image.tmdb.org/t/p/w342/${serie.poster_path}`" alt="" />
             </div>
         </div>
@@ -22,10 +26,21 @@ export default {
 <style scoped>
 .row {
     flex-wrap: nowrap;
+    overflow-x: auto;
+    position: relative;
+}
+
+.col-25 {
+    flex: 0 0 auto;
+}
+
+.col-25 img:last-child {
+    margin-left: ;
 }
 
 img {
-    width: 100%;
+    width: 50%;
     height: auto;
+    object-fit: cover;
 }
 </style>
