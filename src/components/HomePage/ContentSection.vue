299bc10e9fcc250f463104f3f7ac4da5b1dba49f
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-09-18 17:18:19 +0200

    [UPD] added component to cycle all sections

--- src/components/HomePage/ContentSection.vue
+++ src/components/HomePage/ContentSection.vue
@@ -6,28 +6,37 @@ export default {
     data() {
         return {
             store,
-            contents: [],
+            sectionsContents: {}, // Oggetto per contenere i risultati delle varie sezioni
         }
     },
     mounted() {
-        this.getContents()
+        this.getContentsForAllSections()
     },
     methods: {
-        getContents() {
-            axios.get(store.sections[0].apiUrl).then((result) => {
-                this.contents = result.data.results
+        // Metodo per ottenere i contenuti per tutte le sezioni
+        getContentsForAllSections() {
+            store.sections.forEach((section, index) => {
+                axios.get(section.apiUrl).then((result) => {
+                    this.sectionsContents = {
+                        ...this.sectionsContents,
+                        [section.title]: result.data.results,
+                    }
+                })
             })
         },
-        scrollNext() {
-            const row = this.$refs.row
-            const scrollAmount = row.clientWidth // Scorre del 100% della larghezza visibile
-            row.scrollBy({ left: scrollAmount, behavior: "smooth" })
+        scrollNext(index) {
+            const row = this.$refs["row" + index][0]
+            if (row) {
+                const scrollAmount = row.clientWidth
+                row.scrollBy({ left: scrollAmount, behavior: "smooth" })
+            }
         },
-        // Metodo per scorrere indietro di due colonne (100% della riga visibile)
-        scrollPrev() {
-            const row = this.$refs.row
-            const scrollAmount = row.clientWidth // Scorre del 100% della larghezza visibile
-            row.scrollBy({ left: -scrollAmount, behavior: "smooth" })
+        scrollPrev(index) {
+            const row = this.$refs["row" + index][0]
+            if (row) {
+                const scrollAmount = row.clientWidth
+                row.scrollBy({ left: -scrollAmount, behavior: "smooth" })
+            }
         },
     },
 }
@@ -35,21 +44,27 @@ export default {
 
 <template>
     <div class="container">
-        <h3>{{ store.sections[0].title }}</h3>
-        <div class="row-container">
-            <div ref="row" class="row">
-                <div v-for="(movie, index) in contents" :key="movie.id" class="col d-flex">
-                    <img
-                        :src="`http://image.tmdb.org/t/p/w342/${movie.backdrop_path}`"
-                        alt="immagine copertina" />
+        <!-- Ciclo su tutte le sezioni -->
+        <div v-for="(section, index) in store.sections" :key="index">
+            <h3>{{ section.title }}</h3>
+            <div class="row-container">
+                <div :ref="'row' + index" class="row">
+                    <div
+                        v-for="(content, contentIndex) in sectionsContents[section.title]"
+                        :key="content.id"
+                        class="col d-flex">
+                        <img
+                            :src="`http://image.tmdb.org/t/p/w342/${content.backdrop_path}`"
+                            alt="immagine copertina" />
+                    </div>
                 </div>
+                <button class="btn-next" @click="scrollNext(index)">
+                    <i class="fa-solid fa-angle-right"></i>
+                </button>
+                <button class="btn-prev" @click="scrollPrev(index)">
+                    <i class="fa-solid fa-angle-left"></i>
+                </button>
             </div>
-            <button class="btn-next" @click="scrollNext">
-                <i class="fa-solid fa-angle-right"></i>
-            </button>
-            <button class="btn-prev" @click="scrollPrev">
-                <i class="fa-solid fa-angle-left"></i>
-            </button>
         </div>
     </div>
 </template>
@@ -75,18 +90,19 @@ img {
     width: 100%;
     height: auto;
     object-fit: cover;
+    border-radius: 5px;
 }
 
 .btn-prev,
 .btn-next {
     position: absolute;
     transform: translateY(-50%);
-    background-color: rgba(0, 0, 0, 0.861);
+    background: hsla(0, 0%, 8%, 0.5);
     border: 0;
     color: white;
     font-size: var(--font-size-xxl);
     cursor: pointer;
-    height: 100%;
+    height: calc(100% - 10px);
     width: 40px;
     opacity: 0;
     transition: opacity 0.3s ease;
@@ -95,11 +111,24 @@ img {
 .btn-next {
     top: 50%;
     right: 0;
+    border-bottom-left-radius: 4px;
+    border-top-left-radius: 4px;
 }
 
 .btn-prev {
     top: 50%;
     left: 0;
+    border-bottom-right-radius: 4px;
+    border-top-right-radius: 4px;
+}
+
+i {
+    transition: scale 2.3s ease;
+}
+
+i:hover,
+i:hover {
+    transform: scale(1.9);
 }
 
 .row-container:hover .btn-prev,
